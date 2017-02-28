4761d97e2189a455e50d21eac7b8a09280ec305e
vladUng <vvu500@york.ac.uk>
2017-02-28 18:26:32 +0000

    Added UI elements for log in screen

--- Edi/app/src/main/java/com/example/i2lc/edi/Hello.java
+++ Edi/app/src/main/java/com/example/i2lc/edi/Hello.java
@@ -1,13 +1,32 @@
 package com.example.i2lc.edi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class Hello extends AppCompatActivity {
+
+    public final static String EXTRA_USERNAME = "bla";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        Log.i("Edi","Hello 1");
+
     }
+
+    //called when the user clicks the log in button
+    public  void logIn(View view) {
+
+        Intent intent = new Intent(this, PresentationActivity.class);
+        EditText editText = (EditText) findViewById(R.id.username);
+        String username = editText.getText().toString();
+        intent.putExtra(EXTRA_USERNAME, username);
+        startActivity(intent);
+    }
+
 }

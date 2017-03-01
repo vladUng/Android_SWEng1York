33d58590002206c0e05770a7c38deb45435644a9
vladUng <vvu500@york.ac.uk>
2017-03-01 16:31:47 +0000

    Updated the new Android project with the code that I've previously written.

--- Edi/app/src/main/java/com/example/i2lc/edi/LoInActivity.java
+++ Edi/app/src/main/java/com/example/i2lc/edi/LoInActivity.java
@@ -1,13 +1,28 @@
 package com.example.i2lc.edi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoInActivity extends AppCompatActivity {
+
+    public final static String EXTRA_USERNAME = "bla";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lo_in);
+    }
+
+    //called when the user clicks the log in button
+    public  void logIn(View view) {
+
+        Intent intent = new Intent(this, PresentationActivity.class);
+        EditText editText = (EditText) findViewById(R.id.username);
+        String username = editText.getText().toString();
+        intent.putExtra(EXTRA_USERNAME, username);
+        startActivity(intent);
     }
 }

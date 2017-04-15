fe4b80968a54a6dc9d7d74c6465165dc54358612
vladUng <vvu500@york.ac.uk>
2017-04-15 18:40:48 +0300

    Primitive logIn handling implemented

--- Edi/app/src/main/java/com/example/i2lc/edi/LogInActivity.java
+++ Edi/app/src/main/java/com/example/i2lc/edi/LogInActivity.java
@@ -7,13 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.i2lc.edi.backend.UserAuth;
 import com.example.i2lc.edi.backend.socketClient;
+
+import java.util.ArrayList;
 
 public class LogInActivity extends AppCompatActivity {
 
     public final static String EXTRA_USERNAME = "bla";
 
     socketClient mySocketClient;
+
+    private boolean loginSuccessful;
+    private boolean isTeacher;
 
 
     @Override
@@ -26,6 +32,36 @@ public class LogInActivity extends AppCompatActivity {
 
         //Database stuff
         mySocketClient = new socketClient("db.amriksadhra.com", 8080);
+
+        //TODO move this to logiIN action & parse the expected values to it
+        ArrayList<String> userAuthResponse = new ArrayList<String>();
+        userAuthResponse = mySocketClient.userAuth(new UserAuth("userToLogin", "password"));
+
+        if (!userAuthResponse.contains("error")){
+            String authStatus = userAuthResponse.get(0);
+            String userType = userAuthResponse.get(1);
+
+            switch (userType) {
+                case "admin":
+                    loginSuccessful = true;
+                    break;
+                case "teacher":
+                    isTeacher = true;
+                    loginSuccessful = true;
+                    break;
+                case "student":
+                    loginSuccessful = true;
+                    break;
+                case "auth_fail":
+                    loginSuccessful = false;
+                    break;
+            }
+            //for debug
+            System.out.println("LoginSuccessfull: " + loginSuccessful + "\n userType:" + userType);
+        } else {
+            //for debug
+            System.out.println("There was an error");
+        }
     }
 
     //called when the user clicks the log in button
--- Edi/app/src/main/java/com/example/i2lc/edi/backend/socketClient.java
+++ Edi/app/src/main/java/com/example/i2lc/edi/backend/socketClient.java
@@ -9,17 +9,12 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 /**
  * Created by vlad on 06/04/2017.
@@ -41,10 +36,10 @@ public class socketClient {
     private Connection conn;
 
 
-    public void main(String[] args) { new socketClient("127.0.0.1", 8080); }
+    public void main(String[] args) { new socketClient("db.amriksadhra.com", 8080); }
 
     public socketClient(String serverIP, int serverPort) {
-        serverIPAddress = Utils.buildIPAddress("127.0.0.1", serverPort);
+        serverIPAddress = Utils.buildIPAddress("db.amriksadhra.com", serverPort);
 
         connectToRemoteSocket();
         connectToRemoteDB();
@@ -71,34 +66,6 @@ public class socketClient {
 
             System.out.print("Successful connection from client to PostgreSQL database instance \n");
 
-            Statement st = conn.createStatement();
-
-            StringBuilder query = new StringBuilder("SELECT (f).auth_result_return, (f).user_type_return FROM");
-            query.append("(SELECT edi.public.sp_authuser(");
-            query.append("'LoginName',");
-            query.append("'password') as f) AS x;");
-
-            try {
-
-                ResultSet queryResult = st.executeQuery(String.valueOf(query));
-
-                while (queryResult.next()) {
-                    String authStatus  = queryResult.getString("user_type_return");
-                    String userType = queryResult.getString("auth_result_return");
-
-                    System.out.println("\n\n auth status: " + authStatus + " userType: " + userType + "\n\n");
-                }
-
-
-                queryResult.close();
-                st.close();
-            } catch (SQLException e) {
-                System.out.print("There was a sql problem" + e.toString());
-            } catch (Exception e) {
-                System.out.print("There was an unknown problem" + e.toString());
-            }
-
-
         } catch (SQLException e) {
             System.out.print("Unable to connect to PostgreSQL on port 5432");
             System.out.print(e.toString());
@@ -117,98 +84,85 @@ public class socketClient {
         }
 
 
+        //Handling socket events
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 
+            @Override
+            public void call(Object... args) {
+                //TODO understand this part. I think this is used when you want to send some data to the server (?)
+                socket.emit("foo", "hi");
+                socket.disconnect();
+            }
 
+        }).on("event", new Emitter.Listener() {
 
+            @Override
+            public void call(Object... args) {
+                System.out.println("Client connected! Spitting bars.");
+            }
 
+        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
 
+            @Override
+            public void call(Object... args) {
+                System.out.println("For some reason the client is disconnected from the server. Some more info:" + args.toString());
+            }
+        }).on("DB_Update", new Emitter.Listener()  {
 
+            @Override
+            public void call(Object... args) {
 
-//
-//        socket.on(Socket.EVENT_CONNECT, args -> logger.info("Client connected! Spitting bars.")).on("DB_Update", args -> {
-//            System.out.println("Client knows DB has updated:  " + args[0]);
-//
-//            //TODO add the code to deal with this scenario
-//            System.out.println("A table has been updated");
-//        }).on(Socket.EVENT_DISCONNECT, args -> {
-//
-//        });
-//
-//        //Attempt Socket connection
-//        socket.connect();
+                System.out.println("Client knows DB has updated:  " + args[0]);
+                //TODO add the code to deal with this scenario
+            }
+        });
     }
 
 
-    /**
-     * Calls userAuthAsync function but with a LOGIN_TIMEOUT second timeout. If we hit timeout, return false, else wait for server
-     * to respond with response.
-     * @param toAuth User details to authenticate
-     * @return Boolean corrsponding to whether authentication was successful or not
-     * @author Amrik Sadhra
-     */
-    public String userAuth(UserAuth toAuth) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<String> future = executor.submit(new UserAuthTask(toAuth));
+    //handling the authentication request
+    //if successful returns the auth status and user type
+    //otherwise error,error
+    public ArrayList<String> userAuth(UserAuth toAuth) {
 
+        ArrayList<String> retValue = new ArrayList<String>();
+
+        retValue.add("error");
+        retValue.add("error");
+        Statement st = null;
         try {
-            System.out.print("Attempting login of User:" + toAuth.getUserToLogin());
-            return future.get(LOGIN_TIMEOUT, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
-            future.cancel(true);
-            System.out.print("Connection to server timed out.");
-        } catch (InterruptedException e) {
-            System.out.print("Connection to server was interrupted.");
-        } catch (ExecutionException e) {
-            System.out.print("Connection to server failed. (tragically)");
+            st = conn.createStatement();
+
+            StringBuilder query = new StringBuilder("SELECT (f).auth_result_return, (f).user_type_return FROM");
+            query.append("(SELECT edi.public.sp_authuser(");
+            query.append("'LoginName',");
+            query.append("'password') as f) AS x;");
+
+            ResultSet queryResult = st.executeQuery(String.valueOf(query));
+
+            retValue.clear();
+            while (queryResult.next()) {
+                String authStatus  = queryResult.getString("user_type_return");
+                String userType = queryResult.getString("auth_result_return");
+
+                System.out.println("\n\n auth status: " + authStatus + " userType: " + userType + "\n\n");
+                retValue.add(authStatus);
+                retValue.add(userType);
+            }
+
+            queryResult.close();
+            st.close();
+        } catch (SQLException e) {
+            System.out.print("SQL query is wrong" + e.toString());
+            e.printStackTrace();
+
+        } catch (Exception e) {
+            System.out.print("There was an unknown problem" + e.toString());
+            e.printStackTrace();
         }
 
-        //If any of tge catch statement was hit
-        executor.shutdown();
-        return "false";
+        return retValue;
     }
 
 
-    class UserAuthTask implements Callable<String> {
-        UserAuth toAuth;
 
-        public UserAuthTask(UserAuth toAuth){
-            this.toAuth = toAuth;
-        }
-
-        @Override
-        public String call() throws Exception {
-            return userAuthAsync(toAuth);
-        }
-    }
-
-    public String userAuthAsync(UserAuth toAuth) {
-        //Hack to bypass final requirement for Lambda anon methods
-        final FinalWrapper loginSuccessFinal = new FinalWrapper("no_response");
-//
-//        JSONObject obj = new JSONObject();
-//        try {
-//            obj.put("userToLogin", toAuth.getUserToLogin());
-//            obj.put("password", toAuth.getPassword());
-//            socket.emit("AuthUser", obj);
-//            socket.on("AuthUser", objects -> {
-//                if (!(objects[0]).equals("auth_fail")) {
-//                    System.out.print("User " + toAuth.getUserToLogin() + " has successfully logged in");
-//                } else {
-//                    System.out.print("Incorrect username/password for login.");
-//                }
-//
-//                loginSuccessFinal.setNonFinal(objects[0]);
-//            });
-//        } catch (JSONException e) {
-//            System.out.print("Unable to generate JSON object for passing user authentication details. ");
-//        }
-
-        //Spinlock method until our final fake boolean has been modified
-        while(loginSuccessFinal.getNonFinal().equals("no_response")){
-            System.out.print("JVM optimises out empty while loops. Waiting for server response.");
-        }
-
-        //Convert the string we used to store 3 data types in (no resp/true/false) down to boolean now that no resp has been removed as possibility
-        return (String) loginSuccessFinal.getNonFinal();
-    }
 }

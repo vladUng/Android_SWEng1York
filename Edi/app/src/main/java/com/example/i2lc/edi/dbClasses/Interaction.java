a35e39c0b5db6e872064f30c88871fe4e58059d0
cosminfrateanu <sf886@york.ac.uk>
2017-05-31 13:15:39 +0100

    On live interactive element: sending interactive element interaction to db works, tied with the right data.

--- Edi/app/src/main/java/com/example/i2lc/edi/dbClasses/Interaction.java
+++ Edi/app/src/main/java/com/example/i2lc/edi/dbClasses/Interaction.java
@@ -1,5 +1,10 @@
 package com.example.i2lc.edi.dbClasses;
 
+import android.os.Build;
+import android.os.StrictMode;
+
+import com.example.i2lc.edi.backend.SocketClient;
+
 import java.sql.Timestamp;
 
 /**
@@ -23,6 +28,11 @@ public class Interaction {
         this.interactiveElementID = interactiveElementID;
         this.interactionData = interactionData;
         this.timeCreated = timeCreated;
+    }
+    public Interaction(int userID, int interactiveElementID, String interactionData) {
+        this.userID = userID;
+        this.interactiveElementID = interactiveElementID;
+        this.interactionData = interactionData;
     }
 
     public int getInteractionID() {
@@ -63,5 +73,31 @@ public class Interaction {
 
     public void setInteractionData(String interactionData) {
         this.interactionData = interactionData;
+    }
+
+    public void sendInteraction() throws Exception {
+        int SDK_INT = Build.VERSION.SDK_INT;
+        // >SDK 8 support async operations
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            //connect client
+            SocketClient socketClient = new SocketClient();
+            String status = socketClient.postInteraction(userID, interactiveElementID, interactionData);
+
+            if (status.equals("success")) {
+                //for debug
+                System.out.println("YAY the question was successfully sent");
+            } else {
+                //for debug
+                System.out.println("There was an error sending the question to server: " + status);
+            }
+        } else {
+            //for debug
+            System.out.println("There was an error. SDK too old");
+            throw new Exception();
+        }
     }
 }
